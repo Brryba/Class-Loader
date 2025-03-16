ef3a43cd790cc3d04b1f1795cab17e234257422e
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-03-17 02:47:10 +0300

    file loader done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CustomClassLoader loader = new CustomClassLoader();
+        try {
+            Class<?> loadedClass = loader.loadClass("Customer");
+            Object customer = loadedClass.getDeclaredConstructor(String.class).newInstance("Vova");
+            Method getName = loadedClass.getDeclaredMethod("getName");
+            Method setName = loadedClass.getMethod("setName", String.class);
+            System.out.println(getName.invoke(customer));
+            setName.invoke(customer, "Vasya");
+            System.out.println(getName.invoke(customer));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
